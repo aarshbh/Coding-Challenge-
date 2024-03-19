3564d99f769f2643b125311ed1b3d2abc9a31c47
Aarsh Bharti <103813892+aarshbh@users.noreply.github.com>
2024-03-20 00:16:46 +0530

    Add files via upload

---
+++ Day-19/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        '16': '16rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
